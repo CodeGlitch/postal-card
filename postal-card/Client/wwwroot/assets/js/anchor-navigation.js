3af6189bbfaa2118763bdf5147b4f8a3f05189c2
Emanuel Luís <CodeGlitch@users.noreply.github.com>
2023-07-28 17:56:00 +0000

    First commit

---
+++ postal-card/Client/wwwroot/assets/js/anchor-navigation.js
@@ -0,0 +1,11 @@
+// Creator: https://www.meziantou.net/anchor-navigation-in-a-blazor-application.htm
+    function BlazorScrollToId(id) {
+        const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+        }
+    }
